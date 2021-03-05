b0456676a8a2156fcd4e752152c9e62f0b06b464
Jasmine Kay <jasminekay505@gmail.com>
2021-03-04 17:20:53 -0800

    finishing up tests in employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,7 +3,7 @@ const Employee = require('../lib/Employee');
 describe('Employee', () => {
     it('Can create a new employee object', () => {
         const newEmployee = new Employee();
-        expect(typeof(newEmployee)).toBe('object');
+        expect(typeof (newEmployee)).toBe('object');
     });
 
     it('should assign a given string to name via constructor', () => {
@@ -15,28 +15,44 @@ describe('Employee', () => {
     it('should assign a given number to id via constructor', () => {
         const id = 20;
         const newEmployee = new Employee('Jasmine', id);
-        expect(newEmployee(id).toBe(id));
+        expect(newEmployee.id.toBe(id));
     });
 
     it('should assign a given string to email via constructor', () => {
         const email = 'myemail@gmail.com';
         const newEmployee = new Employee('Jasmine', 20, email);
         expect(newEmployee.email).toBe(email);
-    })
+    });
 });
 
-describe('getName', () => { 
-    it('')
+describe('getName', () => {
+    it('should return name of Employee', () => {
+        const name = 'Jasmine';
+        const newEmployee = new Employee(name);
+        expect(newEmployee.getName()).toBe(name);
+    });
 });
 
-describe('getId', () => { 
-    it('')
+describe('getId', () => {
+    it('should return Id of Employee', () => {
+        const id = 20;
+        const newEmployee = new Employee('Jasmine', id);
+        expect(newEmployee.getId()).toBe(id);
+    });
 });
 
-describe('getEmail', () => { 
-    it('')
+describe('getEmail', () => {
+    it('should return email of Employee', () => {
+        const email = 'myemail@gmail.com';
+        const newEmployee = new Employee('Jasmine', 20, email);
+        expect(newEmployee.getEmail()).toBe(email);
+    });
 });
 
-describe('getRole', () => { 
-    it('')
-})
+describe('getRole', () => {
+    it('should return Employee', () => { 
+        const role = 'Employee';
+        const newEmployee = new Employee('Jasmine', 20, 'myemail@gmail.com');
+        expect(newEmployee.getRole()).toBe(role);
+    });
+});
