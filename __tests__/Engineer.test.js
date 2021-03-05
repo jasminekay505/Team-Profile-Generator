5755da16f331d1f1dbbcb5c37198ac17fa839997
Jasmine Kay <jasminekay505@gmail.com>
2021-03-04 17:31:25 -0800

    Adding tests to engineer.test.js

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,25 @@
+const Engineer = require('../lib/Engineer');
+
+describe('Engineer', () => { 
+    it('should assign a given string to gitHub via constructor', () => { 
+        const gitHub = 'jasminekay505';
+        const newEngineer = new Engineer('Jasmine', 20, 'myemail@gmail.com', gitHub);
+        expect(newEngineer.gitHub).toBe(gitHub);
+    });
+});
+
+describe('getGitHub', () => { 
+    it('should return the gitHub user name of Engineer', () => { 
+        const gitHub = 'jasminekay505';
+        const newEngineer = new Engineer('Jasmine', 20, 'myemail@gmail.com', gitHub);
+        expect(newEngineer.getGitHub()).toBe(gitHub);
+    });
+});
+
+describe('getRole', () => {
+    it('should return Engineer', () => { 
+        const role = 'Engineer';
+        const newEmployee = new Employee('Jasmine', 20, 'myemail@gmail.com', 'jasminekay505');
+        expect(newEmployee.getRole()).toBe(role);
+    });
+});
