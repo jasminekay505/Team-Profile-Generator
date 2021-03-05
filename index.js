c12ebce2a6d5a4c2b5e3d847285a0625d51540b5
Jasmine Kay <jasminekay505@gmail.com>
2021-03-05 13:33:09 -0800

    Adding question arrays for manager, intern, engineer

--- index.js
+++ index.js
@@ -3,9 +3,90 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 //Array of questions to gather data
-const questions = [
+const managerQuestions = [
     {
+        type: 'input',
+        message: `What is the team manager's name?`,
+        name: 'managerName'
+    },
+    {
+        type: 'input',
+        message: `What is the team manager's employee ID nummber?`,
+        name: 'managerID'
+    },
+    {
+        type: 'input',
+        message: `What is the team manager's email address?`,
+        name: 'managerEmail'
+    },
+    {
+        type: 'input',
+        message: `What is the team manager's office number?`,
+        name: 'managerOffice'
+    },
 
+];
+
+const addEmployeeQuestion = [
+    {
+        type: 'confirm',
+        message: 'Would you like to add more employees?',
+        name: 'addEmployee'
+    }
+];
+
+const newEmployee = [
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What type of employee would you like to add?',
+        choices: ['Manager', 'Engineer', 'Intern']
+    }
+];
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: `What is the engineers's name?`,
+        name: 'engineerName'
+    },
+    {
+        type: 'input',
+        message: `What is the engineer's employee ID nummber?`,
+        name: 'engineerID'
+    },
+    {
+        type: 'input',
+        message: `What is the engineer's email address?`,
+        name: 'engineerEmail'
+    },
+    {
+        type: 'input',
+        message: `What is the engineer's GitHub username?`,
+        name: 'gitHub'
+    }
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        message: `What is the intern's name?`,
+        name: 'internName'
+    },
+    {
+        type: 'input',
+        message: `What is the intern's employee ID nummber?`,
+        name: 'internID'
+    },
+    {
+        type: 'input',
+        message: `What is the intern's email address?`,
+        name: 'internEmail'
+    },
+    {
+        type: 'input',
+        message:`What school does the intern attend?`,
+        name: 'school'
     }
 ];
 
