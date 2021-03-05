b414c0546d64f96816f0086c59c6fab41e21b158
Jasmine Kay <jasminekay505@gmail.com>
2021-03-04 22:01:51 -0800

    Starting to create inquirer questions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-//
+//Include packages needed
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+//Array of questions to gather data
+const questions = [
+    {
+
+    }
+];
+
+//Function to write HTML file
+function writeToFile(response) { 
+    console.log(`Success! Your new HTML page has been created. It can be found at dist/${response.title}.html`);
+    return renderHTML(reseponse);
+}
+
+//Initialzie App
+function init() {
+    inquirer.prompt(questions).then((response) => { 
+        fs.writeFileSync(`${response.title}-README.md`, writeToFile(response), (err) => { 
+            err ? console.error(err) : console.log('Success! Your README file has been generated!')
+        })
+    })
+ }
+
+// Function call to initialize app
+init();
