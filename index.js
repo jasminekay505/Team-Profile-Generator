f706ed906ec2550d54d98e3a42f0e8d07d4831d0
Jasmine Kay <jasminekay505@gmail.com>
2021-03-05 16:38:40 -0800

    Starting html functions

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function employeeInfo() {
                     }
                     employees.push(newEmployee);
                     console.log(employees);
-                    
+
                     inquirer.prompt(addEmployeeQuestion)
                         .then(({ addEmployee }) => {
                             if (addEmployee) {
@@ -93,6 +93,70 @@ function employeeInfo() {
         });
 }
 
+//Function to write HTML
+function startHTML() { 
+    const main = `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Link to bootstrap cdn -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <!-- Link to fontawesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+        <!-- Link to stylesheet -->
+        <link rel = "stylesheet" href = "style.css">
+        <title>Team Profile</title>
+    </head>
+    
+    <body>
+        <div class="jumbotron jumbotron-fluid text-center">
+            <div class="container">
+                <h1 class="display-4">My Team</h1>
+            </div>
+        </div>
+        <div class="card-deck">
+        </div>`;
 
+        fs.writeFile('./dist/teamProfile.html', main, function(err) { 
+            if(err) { 
+                console.log(err);
+            }
+        });
+}
+
+function addCard(newEmployee) { 
+    const name = newEmployee.getName();
+    const id = newEmployee.getId();
+    const email = newEmployee.getEmail();
+    const role = newEmployee.getRole(); 
+    let card; 
+
+    if (role === 'Manager') {
+        const office = newEmployee.getOfficeNumber();
+        card = `<div class="card">
+        <div class="card-header">
+            ${name}
+            <br>
+            <i class="fas fa-coffee"></i> ${role}
+        </div>
+        <div class="card-body">
+            <p class="cardtext">ID: ${id}</p>
+            <p class="cardtext">Email: <a href="mailto:${email}">${email}</a></p>
+            <p class="cardtext">Office Number: ${office}</p>
+        </div>
+    </div>`
+    }
+    if (role === 'Engineer') {
+        const gitHub = newEmployee.getGitHub();
+    }
+    if (role === 'Intern') {
+        const school = newEmployee.getSchool();
+    }
+
+}
 //Function to start app
 add();
